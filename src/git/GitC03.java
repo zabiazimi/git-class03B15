f4b5c9aa27772d74bfb5c8dcfd8d0a2676ed2736
zabiazimi <zabiazimi1994@gmail.com>
2023-01-10 20:54:09 -0500

    V2

--- src/git/GitC03.java
+++ src/git/GitC03.java
@@ -3,5 +3,6 @@ package git;
 public class GitC03 {
     public static void main(String[] args) {
         System.out.println("uuuuuuuuuuuuu");
+        System.out.println("i love my new job");
     }
 }
